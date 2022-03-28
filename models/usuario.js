dbb9394cde2b3d3575d0b4aac8b9018fde761f91
Victor Balladares <victor.m.balladares.f@gmail.com>
2022-03-28 22:27:30 +0200

    TypeUser

--- models/usuario.js
+++ models/usuario.js
@@ -12,16 +12,15 @@ const pool = new Pool({
 });
 
 //Introducir datos
-
 const guardarUsuario = async (usuario)=>{
-    const {name,email,password} = usuario;
+    const {name,email,password,typeuser} = usuario;
     let client,result;
     try{
         client = await pool.connect(); // Espera a abrir conexion
         const data =  await client.query
-                                    (` INSERT INTO usuarios(name,email,password) 
-                                    VALUES ($1,$2,$3)`
-                                    ,[name,email,password])
+                                    (` INSERT INTO usuarios(name,email,password,typeuser) 
+                                    VALUES ($1,$2,$3,$4)`
+                                    ,[name,email,password,typeuser])
         result = {msg: "Usuario creado exitosamente."}
     }catch(err){
         console.log(err);
@@ -33,6 +32,11 @@ const guardarUsuario = async (usuario)=>{
     }
     return result
 }
+
+
+//Leer usuario
+
+
 
 const usuarios = {
     guardarUsuario
