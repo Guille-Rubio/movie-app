04d46639c39c3c386034d1a22c61e14f99828557
Guille-Rubio <guillermorubiog@gmail.com>
2022-03-29 18:07:58 +0200

    ADD ruta y controlador search y search/:title

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,6 @@
+require('dotenv');
+const fetch = require('node-fetch');
+const API_KEY = process.env.OMDB_API_KEY
 
 
 
@@ -7,31 +10,45 @@ const getMovie = async (req, res) => {
     res.status(200).json(movie);
 }
 
-const createMovie = async (req,res) => {
+const createMovie = async (req, res) => {
     console.log(req.body); // Objeto recibido de entry nueva
     const newMovie = req.body; // {} nueva peli a guardar
     // Líneas para guardar en una BBDD SQL
     const response = await db.createMovie(newMovie);
     console.log(response);
-    res.status(201).json({"items_created":response});
+    res.status(201).json({ "items_created": response });
 }
 
-const getIndex = (req,res)=>{
+const getIndex = (req, res) => {
     res.status(200).render("index");
 }
 
-const getSearchView =(req, res)=>{
+const getSearchView = (req, res) => {
     res.status(200).render("search")
+};
+
+
+const searchMovie = async (req, res) => {
+    const titleSought = req.params.title
+    const response = await fetch(`http://www.omdbapi.com/?t=${titleSought}&apikey=${API_KEY}`)
+    const data = await response.json()
+    const { Title, Year, Runtime, Genre, Director, Actors, Plot, Poster, Ratings } = data;
+
+    res.status(200).render('moviesdetail',{Title:Title,Year:Year,Runtime:Runtime,Genre:Genre,Director:Director,Actors:Actors,Plot,Plot,Poster:Poster,Ratings:Ratings})
+
+
+
 }
+
+
+
 
 const movie = {
     getMovie,
     createMovie,
-    //updateMovie,
-    //deleteMovie,
     getSearchView,
-    getIndex
-
+    getIndex,
+    searchMovie
 }
 
 module.exports = movie;
--- routes/route.js
+++ routes/route.js
@@ -11,10 +11,10 @@ router.get('/dashboard')
 
 // [GET] http://localhost:3000/search
 // Retorna un JSON con los detalles de la peli buscada
-router.get('/search/:title', movie.getMovie);
+//router.get('/search/:title', movie.getMovie);
 
 // [GET] http://localhost:3000/movies
-router.get('/movies', movie.getMovie);
+//router.get('/movies', movie.getMovie);
 
 // [POST] http://localhost:3000/api/entires
 router.post('/entries', movie.createMovie);
@@ -24,6 +24,8 @@ router.post('/entries', movie.createMovie);
 
 router.get('/search',movie.getSearchView)
 
+
+router.get('/search/:title',movie.searchMovie)
 
 
 
