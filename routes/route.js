90f9829d75f4cb72a10eaa17a4c627e78465d805
Guille-Rubio <guillermorubiog@gmail.com>
2022-03-31 23:25:56 +0200

    ADD put editMovie y delete removeMovie

--- routes/route.js
+++ routes/route.js
@@ -1,30 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const movie = require('../controllers/controller')
+const controllers = require('../controllers/controller')
 const passwords = require ('../controllers/passwords')
 
 
 
-router.get('/', movie.getIndex)
-
-router.get('/dashboard',movie.getDashboardView)
-
-router.get('/search', movie.getSearchView)
-
-router.get('/search/:title', movie.searchMovieInOMDB)
-
-router.get('/recoverpassword',movie.getRecuPasswordView)
-
-router.get('/restorepassword',movie.getRestorePasswordView)
-
-router.post('/signup', movie.signup)
+router.get('/', controllers.getIndex)
 
 
-router.post('/createmovie',movie.postCreateMovie)
 
-router.post('/pruebasvictor',movie.pruebasvictor)
+router.get('/dashboard',controllers.getDashboardView)
+
+router.get('/search', controllers.getSearchView)
+
+router.get('/search/:title', controllers.searchMovieInOMDB)
+
+router.get('/recoverpassword',controllers.getRecuPasswordView)
+
+router.get('/restorepassword',controllers.getRestorePasswordView)
+
+router.get('/signup', (req, res) => {
+    res.render('signup.pug')
+})
+
+router.post('/signup', controllers.signup)
 
 
+router.get('/createmovie', (req, res) => {
+    res.render('createmovie.pug')
+})
+
+router.post('/createmovie',controllers.postCreateMovie)
+
+router.post('/pruebasvictor',controllers.pruebasvictor)
+
+router.get('/removemovie', (req, res) => {
+    res.render('removemovie.pug')
+})
+
+router.delete('/removeMovie',controllers.deleteMovie)
+router.put('/editMovie', controllers.editMovie)
+
+router.get('/menu', (req, res) => {
+    res.render('menu.pug')
+})
+router.get('/movies', (req, res) => {
+    res.render('movies.pug')
+})
+
+router.get('/moviesdetail', (req, res) => {
+    res.render('moviesdetail.pug')
+})
+
+router.get('/admin', (req, res) => {
+    res.render('admin.pug')
+})
 
 
 module.exports = router;
