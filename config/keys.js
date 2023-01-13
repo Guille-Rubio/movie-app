5de091cf72d8934fb6aaefaafbd5378d420b0904
Guille-Rubio <guillermorubiog@gmail.com>
2023-01-13 21:10:54 +0100

    commit to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,12 @@
 if (process.env.NODE_ENV === 'production') {
 
     module.exports = require('./prod');
-    console.log("production environment")
+    console.log("production environment");
 
 } else if (process.env.NODE_ENV === 'development') {
 
     module.exports = require('./dev');
-    console.log("development environment")
+    console.log("development environment");
 
 
 }
